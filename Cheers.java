58138219ec3c9e9e55174cb4b4ceaeb4a45b47f9
omerbroshy12 <omerbroshy@gmail.com>
2025-11-05 14:04:17 +0200

    first try

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,26 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+        public static void main(String[] args) 
+        {
+	    String word = args[0];
+            int num = Integer.parseInt(args[1]);
+            char letter;
+            for (int i = 0; i < word.length(); i++)
+            {
+                letter = word.charAt(i);
+                if (letter == 'A' || letter == 'E' || letter == 'F' || letter == 'H' || letter == 'I' || letter == 'L' || letter == 'M' || letter == 'N' || letter == 'O' || letter == 'R' || letter == 'S' || letter == 'X')
+                {
+                   System.out.println("Give me an " + letter + ": " + letter + "!");
+                }
+                else
+                {
+                   System.out.println("Give me a  " + letter + ": " + letter + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for (int j = 0; j < num; j++)
+            {
+                System.out.println(word + "!!!");
+            }
         }
 }
